0ae1153fdc705a524dc3bf23fd71666e4ca92884
nikuman-610 <yuuki.h110929@gmail.com>
2025-01-30 10:23:09 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'Yuuki' && password == 'Yuuki0929'
+    end
+  end
 end
