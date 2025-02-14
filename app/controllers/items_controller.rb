01cfcc44329847f23d1bf5afb48684364988138c
nikuman-610 <yuuki.h110929@gmail.com>
2025-02-14 12:00:14 +0900

    商品出品機能、authenticate_user!導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :summary, :price, :category_id, :condition_id, :charge_id, :prefecture_id,
+                                 :date_id).merge(user_id: current_user.id)
   end
 end
