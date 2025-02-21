cdc1a86df694a46feed37510252a43afb93d3336
nikuman-610 <yuuki.h110929@gmail.com>
2025-02-21 15:47:04 +0900

    購入による条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: :index
   def index
-    @item = Item.find(params[:id])
     @purchase_record_address = PurchaseRecordAddress.new
   end
   
@@ -18,6 +19,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:purchase_record_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).marge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:purchase_record_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless @item.purchase_record.nil? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
