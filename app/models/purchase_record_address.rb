ba7d504b0dc54d90e4e95c84a5a60e6df7e6281a
nikuman-610 <yuuki.h110929@gmail.com>
2025-02-21 15:48:34 +0900

    データベース接続競合解消

--- app/models/purchase_record_address.rb
+++ app/models/purchase_record_address.rb
@@ -5,19 +5,34 @@ class PurchaseRecordAddress
                 :phone_number
   
   with_options presence: true do
-    validates :purchase_record_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "must be in format '###-####'"}
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
-    validates :house_number
-    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Don't include hyphen(-)"}
+    validates :house_number, presence: true
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number with 10 to 11 digits"}
     validates :item_id
     validates :user_id
   end
 
   def save
-    purchase_record = Purchase_record.create(item_id: item_id, user_id: user_id)
-    Address.create(purchase_record_id: purchase_record.id, postal_code: postal_code, city: city, house_number: house_number,
-                   building_name: building_name, phone_number: phone_number)
+    ActiveRecord::Base.transaction do
+      purchase_record = PurchaseRecord.create!(
+        item_id: item_id,
+        user_id: user_id
+      )
+  
+      Address.create!(
+        purchase_record_id: purchase_record.id,
+        postal_code: postal_code,
+        prefecture_id: prefecture_id,
+        city: city,
+        house_number: house_number,
+        building_name: building_name,
+        phone_number: phone_number
+      )
+      true
+    rescue ActiveRecord::RecordInvalid
+      false
+    end
   end
 end
